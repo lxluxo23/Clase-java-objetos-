54e4c5070ee09bb5402c10777f58aa5267d7365c
lxluxo23 <lxluxo23@gmail.com>
2018-10-27 10:37:28 -0300

    a
    a

--- src/ejecutora/control.java
+++ src/ejecutora/control.java
@@ -7,15 +7,14 @@ import modelo.Estudiante;
 import modelo.Estudiantes;
 public class control {
     public static void main(String[] args) {
-        
-       //Coleccion SET
-       System.out.println("Colecciones SET");
-       Estudiantes curso = new Estudiantes();
-       
-       curso.agregar(new Estudiante("19474665-2","luis",18));
-       // Implementacion de HashSet
-        System.out.println("recorriendo Hash");
-        curso.listar();
+      System.out.println("Colecciones SET");
+      Estudiantes curso = new Estudiantes();
+      curso.agregar(new Estudiante("19474665-2","luis",18));
+      System.out.println("recorriendo Hash");
+      
+      
+      
+      curso.listar();
         
         
         
