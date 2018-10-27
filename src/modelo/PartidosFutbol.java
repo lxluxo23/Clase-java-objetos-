e4073de085fa56f55edb8f13057584b79b6e8630
lxluxo23 <lxluxo23@gmail.com>
2018-10-27 15:20:26 -0300

    Update src/modelo/PartidosFutbol.java

--- src/modelo/PartidosFutbol.java
+++ src/modelo/PartidosFutbol.java
@@ -1,9 +1,31 @@
 
 package modelo;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 
 public class PartidosFutbol {
     
+     private List<Partido> alpartidoFutbol;
+
+    public PartidosFutbol() {
+        
+        alpartidoFutbol = new ArrayList<Partido>();
+        
+        
+    }
+    public void agregar(Partido p){
+        alpartidoFutbol.add(p);
+        
+    }
     
+    public void listar(){
+        for(Partido e : alpartidoFutbol){
+            System.out.println("hola  :" +e.getNombre()+"  tu edad es  "+e.getEdad());
+        }
+    }
     
 }
