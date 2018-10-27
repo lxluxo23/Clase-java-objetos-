78dcf7a5d8631a761c9e8bcc80d9c66bdad3cf84
lxluxo23 <lxluxo23@gmail.com>
2018-10-27 10:22:11 -0300

    a
    a

--- src/ejecutora/control.java
+++ src/ejecutora/control.java
@@ -1,15 +1,24 @@
 
 package ejecutora;
-
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import modelo.Estudiante;
 public class control {
     public static void main(String[] args) {
         
-        System.out.println("     HashSet ");
+       //Coleccion SET
+       System.out.println("Colecciones SET");
+       Estudiante est = new Estudiante("195980101", "Luis Yañez",21 );
+       Estudiante est2 = new Estudiante("99999999", "Luis Yañez2",21 );
+       Estudiante est3 = new Estudiante("11111111", "Luis Yañez3",21 );
+       // Implementacion de HashSet
+        System.out.println("     HashSet");
+        Set<Estudiante> hsEstudiante = new HashSet<>();//HashSet es el  subfuncion de SET
         
-        set<Estudiante> hsEstudiante
+        System.out.println("recorriendo Hash");
         
-        
-        
+        Iterator iterator = hsEstudiante.iterator();
         
         
         
