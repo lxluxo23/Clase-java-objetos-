dc74f7780e00527288791bb265b5f47c8dae627b
lxluxo23 <lxluxo23@gmail.com>
2018-10-27 15:12:38 -0300

    Update src/modelo/Partido.java

---
+++ src/modelo/Partido.java
@@ -0,0 +1,11 @@
+
+package modelo;
+
+
+public class Partido {
+    private String equipolocal;
+    private String equipovisitante;
+    private int goleslocal;
+    private int golesvisitantes;
+   
+}
