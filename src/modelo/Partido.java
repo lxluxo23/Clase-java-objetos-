ab8a759611bad325d4d107b38cf0ea222c008efe
lxluxo23 <lxluxo23@gmail.com>
2018-10-27 15:15:11 -0300

    Update src/modelo/Partido.java

--- src/modelo/Partido.java
+++ src/modelo/Partido.java
@@ -1,11 +1,49 @@
-
 package modelo;
 
-
 public class Partido {
+
     private String equipolocal;
     private String equipovisitante;
     private int goleslocal;
     private int golesvisitantes;
-   
+
+    public Partido(String equipolocal, String equipovisitante, int goleslocal, int golesvisitantes) {
+        this.equipolocal = equipolocal;
+        this.equipovisitante = equipovisitante;
+        this.goleslocal = goleslocal;
+        this.golesvisitantes = golesvisitantes;
+    }
+
+    public String getEquipolocal() {
+        return equipolocal;
+    }
+
+    public void setEquipolocal(String equipolocal) {
+        this.equipolocal = equipolocal;
+    }
+
+    public String getEquipovisitante() {
+        return equipovisitante;
+    }
+
+    public void setEquipovisitante(String equipovisitante) {
+        this.equipovisitante = equipovisitante;
+    }
+
+    public int getGoleslocal() {
+        return goleslocal;
+    }
+
+    public void setGoleslocal(int goleslocal) {
+        this.goleslocal = goleslocal;
+    }
+
+    public int getGolesvisitantes() {
+        return golesvisitantes;
+    }
+
+    public void setGolesvisitantes(int golesvisitantes) {
+        this.golesvisitantes = golesvisitantes;
+    }
+
 }
